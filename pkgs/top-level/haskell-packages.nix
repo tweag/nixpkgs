7d1dc7ee4f07c121e696d38770558ec1cd4aee01
Utku Demir <me@utdemir.com>
2020-12-15 11:57:23 +1300

    Remove unnecessary overrides after the rebase

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -111,15 +111,10 @@ in {
       llvmPackages = pkgs.llvmPackages_10;
     };
     ghcHEAD = callPackage ../development/compilers/ghc/head.nix {
-      bootPkgs = packages.ghc883.override { # no binary yet
-        overrides = self: _: {
-          happy = self.happy_1_20_0;
-        };
-      };
+      bootPkgs = packages.ghc8102Binary;
       inherit (buildPackages.python3Packages) sphinx;
       buildLlvmPackages = buildPackages.llvmPackages_10;
       llvmPackages = pkgs.llvmPackages_10;
-      libffi = pkgs.libffi;
     };
     ghcjs = compiler.ghcjs86;
     ghcjs86 = callPackage ../development/compilers/ghcjs-ng {
