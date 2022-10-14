8f158a501b62b7247aecce461d57cc5211e23463
Silvan Mosberger <contact@infinisil.com>
2022-10-14 21:25:04 +0200

    dependencyInfo: Use runCommandCC
    Also add tracing and some other small improvements

--- pkgs/build-support/cc/c2nix/dependencyInfo.nix
+++ pkgs/build-support/cc/c2nix/dependencyInfo.nix
@@ -2,9 +2,9 @@
 TODO: Output JSON instead of .d file
 */
 {
-stdenv,
 jq,
 writeShellScriptBin,
+runCommandCC,
 }:
 {
 name,
@@ -14,11 +14,9 @@ all_include_dirs,
 preprocessor_flags,
 }:
 let
-  all_src = src;
-
   include_path = toString (
-      map (inc: "-I ${inc}") all_include_dirs
-      );
+    map (inc: "-I ${inc}") all_include_dirs
+  );
 
   processFile = writeShellScriptBin "process" ''
     set -euo pipefail
@@ -30,6 +28,8 @@ let
     json_file=$out/$source_file.json
     mkdir -p "$(dirname "$out/$source_file")"
     [[ $source_file =~ .*\.c$ ]] && COMPILER=$CC || COMPILER=$CXX
+
+    echo "Processing $source_file with $COMPILER to $json_file" >&2
 
     # -M to output make rule of the files dependencies
     # -MM instead to not include system libraries, limits it to only the projects files
@@ -52,55 +52,49 @@ let
     # raising an error.  The dependency filename is taken directly from
     # the "#include" directive without prepending any path.
   '';
-in
 # Dependency information - built at instantiation time!
-stdenv.mkDerivation {
-    name = "${name}.depinfo";
+in runCommandCC "${name}.depinfo" {
 
-    # TODO: can we use `src` and `unpack = false`?
-    inherit all_src;
+  # Note: these need to be separate outputs or else using nix to read the contents of the modules
+  #  may (in nix circa 2.7) bring along the context of the dependencies of the other files
+  #  which can cause a variety of problems without helping us any.
+  # -- Lagoda (with Dave over my shoulder) 2022-03-22
 
-    buildInputs = includeInputs;
+  # out: one dependency info file per source file, containing Make rules
+  # modules: file containing list of source files, one per line
+  outputs = ["out" "modules"];
 
-    phases = ["build"];
+  nativeBuildInputs = [
+    jq
+    processFile
+  ];
 
-    # Note: these need to be separate outputs or else using nix to read the contents of the modules
-    #  may (in nix circa 2.7) bring along the context of the dependencies of the other files
-    #  which can cause a variety of problems without helping us any.
-    # -- Lagoda (with Dave over my shoulder) 2022-03-22
+  # TODO: Still needed with -MM?
+  buildInputs = includeInputs;
 
-    # out: one dependency info file per source file, containing Make rules
-    # modules: file containing list of source files, one per line
-    outputs = ["out" "modules"];
+  passAsFile = [ "sedScript" ];
 
-    passAsFile = [ "sedScript" ];
+  sedScript = ''
+    # `make` wraps lines with "\", adding extra spaces around it, separating entries
+    s/ \\\n /\n/g
+    # On smaller lines, entries are separated by " ", make those newlines too
+    s/ /\n/g
+    # But now we also replaced "\ " with newlines, which `make` uses to escape " ", so undo those!
+    s/\\\n/ /g
+    # `make` escapes "#" with "\#"
+    s/\\#/#/g
+    # And "%" with "%%"
+    s/%%/%/g
+    # Similarly "$" with "$$"
+    s/\$\$/$/g
+  '';
+} ''
+  mkdir $out
+  touch $modules
 
-    nativeBuildInputs = [
-      jq
-      processFile
-    ];
+  cd ${src}
 
-    sedScript = ''
-      # `make` wraps lines with "\", adding extra spaces around it, separating entries
-      s/ \\\n /\n/g
-      # On smaller lines, entries are separated by " ", make those newlines too
-      s/ /\n/g
-      # But now we also replaced "\ " with newlines, which `make` uses to escape " ", so undo those!
-      s/\\\n/ /g
-      # `make` escapes "#" with "\#"
-      s/\\#/#/g
-      # And "%" with "%%"
-      s/%%/%/g
-      # Similarly "$" with "$$"
-      s/\$\$/$/g
-    '';
-
-    build = ''
-      mkdir $out
-      cd $all_src
-      find -L . -type f \( -name '*.c' -or -name '*.cpp' -or -name '*.cc' \) -print0 | sort -z \
-        | xargs -0 -L1 -I{} -P "$NIX_BUILD_CORES" process {} ${preprocessor_flags} ${include_path}
-
-      # TODO: Escaping of preprocessor_flags and include_path, both for shell and xargs
-    '';
-}
+  # TODO: Escaping of preprocessor_flags and include_path, both for shell and xargs
+  find -L . -type f \( -name '*.c' -or -name '*.cpp' -or -name '*.cc' \) -print0 | sort -z \
+    | xargs -0 -L1 -I{} -P "$NIX_BUILD_CORES" process {} ${preprocessor_flags} ${include_path}
+''
