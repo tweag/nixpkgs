f7c3b9c1cfa2c6b58bed6cb43f663aacaae84726
Valentin Gagarin <valentin.gagarin@tweag.io>
2022-10-19 03:48:29 +0200

    document `dependencyInfo` parameters

--- pkgs/build-support/cc/c2nix/dependencyInfo.nix
+++ pkgs/build-support/cc/c2nix/dependencyInfo.nix
@@ -3,18 +3,71 @@
   jq,
   writeShellScriptBin,
   runCommandCC,
-}: {
+}:
+/*
+Produce a JSON file describing dependencies between C or C++ source files within the given `src` directory.
+
+Example:
+
+```shellSession
+tree src
+```
+
+    src
+    ├── lib
+    │   ├── util.c
+    │   └── util.h
+    └── main.cpp
+
+
+```shellSession
+nix-build <nixpkgs> -A pkgs.c2nix.dependencyInfo --argstr name example --argsr src $PWD/src
+```
+
+    /nix/store/y1m9xhvissgjvzkzjxmrqg7cmmpr5qbh-example-depinfo.json
+
+
+```shellSession
+cat /nix/store/y1m9xhvissgjvzkzjxmrqg7cmmpr5qbh-example-depinfo.json
+```
+
+    [
+      {
+        "name": "./lib/util.c",
+        "dependencies": [
+          "lib/util.c"
+        ]
+      },
+      {
+        "name": "./main.cpp",
+        "dependencies": [
+          "lib/util.h",
+          "main.cpp"
+        ]
+      }
+    ]
+
+*/
+{
+  # Package name
   name,
+  # Source directory to analyse for dependencies
   src,
+  # Flags to pass to the analysing compiler
+  #
+  # Type:
+  #   [String]
   compilerFlags ? [],
+  # File extensions to recognise as C files
   cExtensions ? ["c"],
+  # File extensions to recognise as C++ files
   cppExtensions ? ["cpp" "cc"],
 }:
 assert (
   let
     intersection = lib.intersectLists cExtensions cppExtensions;
   in
-    lib.assertMsg (intersection == []) "c2nix.dependencyInfo: File extensions ${lib.head intersection} cannot be in both cExtensions and cppExtensions."
+    lib.assertMsg (intersection == []) "c2nix.dependencyInfo: File extensions ${lib.head intersection} cannot be in both `cExtensions` and `cppExtensions`."
 );
 # TODO: Detect extra files in src that aren't a dependency of any module? These aren't too surprising (e.g. an include directory for a
 # library that this program only uses part of) so I'm not sure it's worth it.
@@ -47,7 +100,7 @@ assert (
 
       echo "Determining file dependencies of $source_file with $COMPILER" >&2
 
-      # Idea: Don't use -MM, but -M, such that dependencies on /nix/store
+      # TODO: Idea: Don't use -MM, but -M, such that dependencies on /nix/store
       # libraries are shown, then make sure to have these libraries as a build
       # input for the files that require them. This way, when those libraries
       # change, only part of the files need to be recompiled.
@@ -59,11 +112,11 @@ assert (
       # existing, and we shouldn't write store paths to a JSON file because it
       # couldn't be committed to nixpkgs.
 
-      # -M to output make rule of the files dependencies
-      # -MM instead to not include system libraries, limits it to only the projects files
-      # -MT to specify the make rule target string, let it be fixed because we don't need to know it
+      # -M to output a Make rule for the file's dependencies
+      # -MM to exclude system libraries, limits it to only the projects files.
+      # -MT to pecify the Make rule target string. Let it be `fixed` because we don't need to know it.
       $COMPILER -MM -MT fixed "$source_file" ${lib.escapeShellArgs compilerFlags} \
-        | # Unescape makefile escapings \
+        | # Unescape Make escapings \
         sed -z -f "$sedScriptPath" \
         | # Remove the first line containing "fixed:" \
         tail -n+2 \
@@ -73,8 +126,8 @@ assert (
         jq --raw-input -s --arg path "$source_file" '{ name: $path, dependencies: rtrimstr("\n") | split("\n") }' \
         > "$result_dir"/"$source_file.json"
     '';
-    # Dependency information - built at instantiation time!
   in
+    # Dependency information - built at instantiation time!
     runCommandCC "${name}-depinfo.json" {
       nativeBuildInputs = [
         jq
@@ -83,13 +136,13 @@ assert (
 
       passAsFile = ["sedScript"];
 
-      # Undoes make rule escaping for a single rule, outputting all dependents on a single line
+      # Undo Make rule escaping for a single rule, output all dependencies on a single line
       sedScript = ''
-        # `make` wraps lines with "\", adding extra spaces around it, separating entries
+        # Make wraps lines with "\", adding extra spaces around it, separating entries
         s/ \\\n /\n/g
         # On smaller lines, entries are separated by " ", make those newlines too
         s/ /\n/g
-        # But now we also replaced "\ " with newlines, which `make` uses to escape " ", so undo those!
+        # But now we also replaced "\ " with newlines, which Make uses to escape " ", so undo those!
         s/\\\n/ /g
         # `make` escapes "#" with "\#"
         s/\\#/#/g
@@ -111,5 +164,4 @@ assert (
       find "$results" -type f -print0 \
         | xargs -0 cat \
         | jq --slurp '.' > $out
-
     ''
