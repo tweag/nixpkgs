b28fe1791f408bc3d3444b1f2b3ccb4c9c03ffad
Valentin Gagarin <valentin.gagarin@tweag.io>
2022-11-06 00:13:19 +0100

    dependencyInfo: add example, refactor for readability

--- pkgs/build-support/cc/c2nix/internal/dependencyInfo.nix
+++ pkgs/build-support/cc/c2nix/internal/dependencyInfo.nix
@@ -64,10 +64,31 @@ pkgs.lib.importJSON dependencies
     [ { dependencies = [ "lib/util.c" ]; name = "./lib/util.c"; } { dependencies = [ "lib/util.h" "main.cpp" ]; name = "./main.cpp"; } ]
 
 
-TODO: Example like this:
-nix-build -E 'dependencyInfo { ... }'
+Example:
+
+To avoid "Import From Derivation", build the dependency information file independently, and then use it like a regular source file.
+
+```nix
+# dependencies.nix
+with import <nixpkgs> {};
+c2nix.dependencyInfo { name = "example"; src = ./source; };
+```
+
+```shellSession
+nix-build dependencies.nix
+```
+
+    /nix/store/y1m9xhvissgjvzkzjxmrqg7cmmpr5qbh-example-depinfo.json
+
+```shellSession
 cp result dependency-info.json
-nix-build -E 'buildCPP { dependencyInfo = ./dependency-info.json; }' # Not IFD anymore
+```
+
+```nix
+# project.nix
+with import <nixpkgs> {};
+c2nix.buildCPP { src = ./source; dependencyInfo = ./dependency-info.json; }'
+```
 
 */
 {
@@ -88,102 +109,107 @@ nix-build -E 'buildCPP { dependencyInfo = ./dependency-info.json; }' # Not IFD a
 assert (
   let
     intersection = lib.intersectLists cExtensions cppExtensions;
+    extensions = lib.strings.concatStringsSep " " intersection;
   in
-    lib.assertMsg (intersection == []) "c2nix.dependencyInfo: File extensions ${lib.head intersection} cannot be in both `cExtensions` and `cppExtensions`."
+    lib.assertMsg (intersection == []) "c2nix.dependencyInfo: File extensions ${extensions} cannot be in both `cExtensions` and `cppExtensions`."
 );
-# TODO: Detect extra files in src that aren't a dependency of any module? These aren't too surprising (e.g. an include directory for a
-# library that this program only uses part of) so I'm not sure it's worth it.
-  let
-    processFile = writeShellScriptBin "process" ''
-      set -euo pipefail
+# FIXME: Detect extra files in src that aren't a dependency of any module?
+# These aren't too surprising (e.g. an include directory for a library that
+# this program only uses part of) so I'm not sure it's worth it.
+let
+  findDependencies =  ''
+    results=$(mktemp -d)
 
-      result_dir=$1
-      shift
-      source_file=$1
-      shift
+    cd ${src}
 
-      # Remove longest prefix matching "*.", resulting in everything after the
-      # last dot
-      extension=''${source_file##*.}
+    # Recursively find all files and process them in parallel using the `processFile` script
+    find -L . -type f -print0 \
+      | xargs -0 -L1 -P "$NIX_BUILD_CORES" processFile "$results"
 
-      case "$extension" in
-      ${lib.concatMapStringsSep " | " lib.escapeShellArg cExtensions})
-        COMPILER=$CC
-        ;;
-      ${lib.concatMapStringsSep " | " lib.escapeShellArg cppExtensions})
-        COMPILER=$CXX
-        ;;
-      *)
-        echo "Skipping non-source file $source_file" >&2
-        exit 0
-      esac
+    # Combine all resulting JSON files into a single one
+    find "$results" -type f -print0 \
+      | xargs -0 cat \
+      | jq --slurp '{ dependencies: . | from_entries }' > $out
+  '';
 
-      mkdir -p "$(dirname "$result_dir/$source_file")"
+  processFile = ''
+    result_dir=$1
+    shift
+    source_file=$1
+    shift
 
-      echo "Determining file dependencies of $source_file with $COMPILER" >&2
+    # Remove longest prefix matching "*.", resulting in everything after the
+    # last dot
+    extension=''${source_file##*.}
 
-      # TODO: Idea: Don't use -MM, but -M, such that dependencies on /nix/store
-      # libraries are shown, then make sure to have these libraries as a build
-      # input for the files that require them. This way, when those libraries
-      # change, only part of the files need to be recompiled.
-      # Maybe how to:
-      # - Output all store paths in a separate attribute
-      # - Use builtins.storePath to attach the corresponding string context
-      # - Make the paths a `buildInput` for the individual modules compilation
-      # Problem: This doesn't work, because we can't rely on these store paths
-      # existing, and we shouldn't write store paths to a JSON file because it
-      # couldn't be committed to nixpkgs.
+    case "$extension" in
+    ${lib.concatMapStringsSep " | " lib.escapeShellArg cExtensions})
+      COMPILER=$CC
+      ;;
+    ${lib.concatMapStringsSep " | " lib.escapeShellArg cppExtensions})
+      COMPILER=$CXX
+      ;;
+    *)
+      echo "Skipping non-source file $source_file" >&2
+      exit 0
+    esac
 
-      # -M to output a Make rule for the file's dependencies
-      # -MM to exclude system libraries, limits it to only the projects files.
-      # -MT to pecify the Make rule target string. Let it be `fixed` because we don't need to know it.
-      $COMPILER -MM -MT fixed "$source_file" ${lib.escapeShellArgs compilerFlags} \
-        | # Unescape Make escapings \
-        sed -z -f "$sedScriptPath" \
-        | # Remove the first line containing "fixed:" \
-        tail -n+2 \
-        | # Sort and remove duplicates, which GCC apparently produces \
-        sort -u \
-        | # Turn lines into a JSON array \
-        jq --raw-input -s --arg path "$source_file" '{ key: $path, value: rtrimstr("\n") | split("\n") }' \
-        > "$result_dir"/"$source_file.json"
-    '';
-  in
-    # Dependency information - built at instantiation time!
-    runCommandCC "${name}-depinfo.json" {
-      nativeBuildInputs = [
-        jq
-        processFile
-      ];
+    mkdir -p "$(dirname "$result_dir/$source_file")"
 
-      passAsFile = ["sedScript"];
+    echo "Determining file dependencies of $source_file with $COMPILER" >&2
 
-      # Undo Make rule escaping for a single rule, output all dependencies on a single line
-      sedScript = ''
-        # Make wraps lines with "\", adding extra spaces around it, separating entries
-        s/ \\\n /\n/g
-        # On smaller lines, entries are separated by " ", make those newlines too
-        s/ /\n/g
-        # But now we also replaced "\ " with newlines, which Make uses to escape " ", so undo those!
-        s/\\\n/ /g
-        # `make` escapes "#" with "\#"
-        s/\\#/#/g
-        # And "%" with "%%"
-        s/%%/%/g
-        # Similarly "$" with "$$"
-        s/\$\$/$/g
-      '';
-    } ''
-      results=$(mktemp -d)
+    # -M to output a Make rule for the file's dependencies
+    # -MM to exclude system libraries, limits it to only the projects files.
+    # -MT to pecify the Make rule target string. Let it be `fixed` because we don't need to know it.
 
-      cd ${src}
+    # FIXME: Don't use -MM, but -M, such that dependencies on /nix/store
+    # libraries are shown, then make sure to have these libraries as a build
+    # input for the files that require them. This way, when those libraries
+    # change, only part of the files need to be recompiled.
+    # Possible implementation:
+    # - Output all store paths in a separate attribute
+    # - Use builtins.storePath to attach the corresponding string context
+    # - Make the paths a `buildInput` for the individual modules compilation
+    # Problem: This doesn't work, because we can't rely on these store paths
+    # existing, and we shouldn't write store paths to a JSON file because it
+    # couldn't be committed to Nixpkgs.
 
-      # Recursively find all files and process them in parallel using the process script
-      find -L . -type f -print0 \
-        | xargs -0 -L1 -P "$NIX_BUILD_CORES" process "$results"
+    $COMPILER -MM -MT fixed "$source_file" ${lib.escapeShellArgs compilerFlags} \
+      | # Undo Make escapings, split dependencies into lines \
+      sed -z -f "$processDependenciesPath" \
+      | # Remove the first line containing "fixed:" \
+      tail -n+2 \
+      | # Sort and remove duplicates, which GCC apparently produces \
+      sort -u \
+      | # Turn lines into a JSON array \
+      jq --raw-input -s --arg path "$source_file" '{ key: $path, value: rtrimstr("\n") | split("\n") }' \
+      > "$result_dir"/"$source_file.json"
+  '';
 
-      # Combine all resulting JSON files into a single one
-      find "$results" -type f -print0 \
-        | xargs -0 cat \
-        | jq --slurp '{ dependencies: . | from_entries }' > $out
-    ''
+  processDependencies = ''
+    # Undo Make rule escaping for a single rule and output each dependency
+    # on a single line
+
+    # Make wraps lines with "\", adding extra spaces around it, separating entries
+    s/ \\\n /\n/g
+    # On smaller lines, entries are separated by " ", make those newlines too
+    s/ /\n/g
+    # But now we also replaced "\ " with newlines, which Make uses to escape " ", so undo those!
+    s/\\\n/ /g
+    # `make` escapes "#" with "\#"
+    s/\\#/#/g
+    # And "%" with "%%"
+    s/%%/%/g
+    # Similarly "$" with "$$"
+    s/\$\$/$/g
+  '';
+in
+runCommandCC "${name}-depinfo.json" {
+  nativeBuildInputs = [
+    jq
+    (writeShellApplication "processFile" processFile)
+  ];
+
+  inherit processDependencies;
+  passAsFile = ["processDependencies"];
+} findDependencies
