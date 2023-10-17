6db1ae7f9a3d3ea87383d8c4aceca2f27888aed6
Silvan Mosberger <silvan.mosberger@tweag.io>
2023-10-18 01:13:00 +0200

    This is mergeable

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@ let requiredVersion = import ./lib/minver.nix; in
 
 if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.nixVersion == 1 then
 
+
   abort ''
 
     This version of Nixpkgs requires Nix >= ${requiredVersion}, please upgrade:
