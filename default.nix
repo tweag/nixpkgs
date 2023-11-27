b92a3363c288a72dd357c42e6cb2a1e37a6218f9
Silvan Mosberger <silvan.mosberger@tweag.io>
2023-11-27 22:46:48 +0100

    default.nix change

--- default.nix
+++ default.nix
@@ -11,9 +11,6 @@ if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.
     - Alternatively, with Nix > 2.0 `nix upgrade-nix' can be used to imperatively
       upgrade Nix. You may use `nix-env --version' to check which version you have.
 
-    - If you installed Nix using the install script (https://nixos.org/nix/install),
-      it is safe to upgrade by running it again:
-
           curl -L https://nixos.org/nix/install | sh
 
     For more information, please see the NixOS release notes at
