d4ebfad494647f617a7f03295c7102aeb42d2b09
Silvan Mosberger <silvan.mosberger@tweag.io>
2024-04-26 23:06:17 +0200

    A change

--- default.nix
+++ default.nix
@@ -3,6 +3,7 @@ let requiredVersion = import ./lib/minver.nix; in
 if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.nixVersion == 1 then
 
   abort ''
+    Foo
 
     This version of Nixpkgs requires Nix >= ${requiredVersion}, please upgrade:
 
