f2d312a4e4e17cbdca2743391b5d13d88cdb91c4
Jörg Thalheim <joerg@thalheim.io>
2024-11-16 08:11:33 +0100

    ci/parallel.nix: nixfmt

--- ci/parallel.nix
+++ ci/parallel.nix
@@ -1,26 +1,25 @@
 /*
-Invocation:
+  Invocation:
 
+  Invocation; note that the number of processes spawned is four times
+  the number of cores -- this helps in two ways:
 
-Invocation; note that the number of processes spawned is four times
-the number of cores -- this helps in two ways:
+  1. Keeping cores busy while I/O operations are in flight
 
-1. Keeping cores busy while I/O operations are in flight
-
-2. Since the amount of time needed for the jobs is *not* balanced
-   this minimizes the "tail latency" for the very last job to finish
-   (on one core) by making the job size smaller.
-
+  2. Since the amount of time needed for the jobs is *not* balanced
+     this minimizes the "tail latency" for the very last job to finish
+     (on one core) by making the job size smaller.
 */
 # see pkgs/top-level/nohydra
-{ lib ? import ../lib
-, checkMeta
-, includeBroken ? true
-, path ? ./..
-, systems
-, myChunk
-, numChunks
-, attrPathFile
+{
+  lib ? import ../lib,
+  checkMeta,
+  includeBroken ? true,
+  path ? ./..,
+  systems,
+  myChunk,
+  numChunks,
+  attrPathFile,
 }:
 
 let
@@ -28,34 +27,37 @@ let
   chunkSize = (lib.length attrPaths) / numChunks;
   myPaths =
     let
-      dropped = lib.drop (chunkSize*myChunk) attrPaths;
+      dropped = lib.drop (chunkSize * myChunk) attrPaths;
     in
-      if myChunk == numChunks - 1
-      then dropped
-      else lib.take chunkSize dropped;
+    if myChunk == numChunks - 1 then dropped else lib.take chunkSize dropped;
 
   unfiltered = import ../pkgs/top-level/release-outpaths.nix {
-    inherit checkMeta path includeBroken systems;
+    inherit
+      checkMeta
+      path
+      includeBroken
+      systems
+      ;
   };
 
-  f = i: m: a:
-    lib.mapAttrs (name: values:
+  f =
+    i: m: a:
+    lib.mapAttrs (
+      name: values:
       if a ? ${name} then
-        if lib.any (value: lib.length value <= i + 1) values then
-          a.${name}
-        else
-          f (i + 1) values a.${name}
+        if lib.any (value: lib.length value <= i + 1) values then a.${name} else f (i + 1) values a.${name}
       else
         null
     ) (lib.groupBy (a: lib.elemAt a i) m);
 
   filtered = f 0 myPaths unfiltered;
 
-  recurseEverywhere = val:
-    if lib.isDerivation val || !(lib.isAttrs val)
-    then val
-    else (builtins.mapAttrs (_: v: recurseEverywhere v) val)
-         // { recurseForDerivations = true; };
+  recurseEverywhere =
+    val:
+    if lib.isDerivation val || !(lib.isAttrs val) then
+      val
+    else
+      (builtins.mapAttrs (_: v: recurseEverywhere v) val) // { recurseForDerivations = true; };
 
 in
-  recurseEverywhere filtered
+recurseEverywhere filtered
