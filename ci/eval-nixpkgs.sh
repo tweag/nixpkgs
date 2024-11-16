7d887a5c04b998d5e3fb1b40d9efb228aff1d06a
Jörg Thalheim <joerg@thalheim.io>
2024-11-16 08:24:35 +0100

    ci/eval: reformat with shfmt
    the file was using mixed indentation (2 vs 4 spaces)

--- ci/eval-nixpkgs.sh
+++ ci/eval-nixpkgs.sh
@@ -4,22 +4,22 @@
 set -euxo pipefail
 
 system="x86_64-linux"
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 NIXPKGS_PATH="$(readlink -f "$SCRIPT_DIR"/..)"
 
 parseArgs() {
-  while [[ $# -gt 0 ]]; do
-    case $1 in
-      --system)
-        system=$2
-        shift 2
-        ;;
-      *)
-        echo "Unknown argument: $1"
-        exit 1
-        ;;
-    esac
-  done
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+        --system)
+            system=$2
+            shift 2
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+        esac
+    done
 }
 
 main() {
@@ -27,7 +27,7 @@ main() {
     tmpdir=$(mktemp -d)
     trap 'rm -rf "$tmpdir"' EXIT
 
-    nix-instantiate --eval --strict --json --arg enableWarnings false "$NIXPKGS_PATH"/pkgs/top-level/release-attrpaths-superset.nix -A paths > "$tmpdir/paths.json"
+    nix-instantiate --eval --strict --json --arg enableWarnings false "$NIXPKGS_PATH"/pkgs/top-level/release-attrpaths-superset.nix -A paths >"$tmpdir/paths.json"
 
     CORES=$(nproc)
     # Originally @amjoseph: note that the number of processes spawned is four times
@@ -36,18 +36,17 @@ main() {
     # 2. Since the amount of time needed for the jobs is *not* balanced
     # this minimizes the "tail latency" for the very last job to finish
     # (on one core) by making the job size smaller.
-    NUM_CHUNKS=$(( 4 * CORES ))
-
+    NUM_CHUNKS=$((4 * CORES))
 
     (
-      set +e
-      parallel -j "$CORES" \
-          nix-env -qaP --no-name --out-path --arg checkMeta true --arg includeBroken true \
-          --arg systems "[\"$system\"]" \
-          -f "$NIXPKGS_PATH"/ci/parallel.nix --arg attrPathFile "$tmpdir"/paths.json \
-          --arg numChunks "$NUM_CHUNKS" --show-trace --arg myChunk \
-          -- $(seq 0 $(( NUM_CHUNKS - 1 ))) > "$tmpdir/paths"
-      echo $? > "$tmpdir/exit-code"
+        set +e
+        parallel -j "$CORES" \
+            nix-env -qaP --no-name --out-path --arg checkMeta true --arg includeBroken true \
+            --arg systems "[\"$system\"]" \
+            -f "$NIXPKGS_PATH"/ci/parallel.nix --arg attrPathFile "$tmpdir"/paths.json \
+            --arg numChunks "$NUM_CHUNKS" --show-trace --arg myChunk \
+            -- $(seq 0 $((NUM_CHUNKS - 1))) >"$tmpdir/paths"
+        echo $? >"$tmpdir/exit-code"
     ) &
     pid=$!
     while kill -0 "$pid"; do
