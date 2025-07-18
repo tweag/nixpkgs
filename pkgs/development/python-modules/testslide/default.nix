1798577596d0ef1662a1eeb639ad93c041481712
Johan Herland <johan@herland.net>
2025-07-18 19:17:52 +0000

    WIP: Not sure I can find a version to make this work...

--- pkgs/development/python-modules/testslide/default.nix
+++ pkgs/development/python-modules/testslide/default.nix
@@ -5,26 +5,34 @@
   coverage,
   psutil,
   pygments,
-  pytestCheckHook,
+  pytest,
+  pytest-asyncio,
   setuptools,
   typeguard,
 }:
 
 buildPythonPackage rec {
   pname = "TestSlide";
-  version = "2.7.1-unstable-2025-01-17";
+  version = "2.7.1-unstable-2025-01-17"; # this main branch has a .bdd submodule that does not make it into the wheel!
+  # version = "2.7.1";  # fails due to typeguard.qualified_name!
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "facebook";
     repo = "TestSlide";
+    # tag = version;
+    # hash = "sha256-M/qUhzbQzm3D6P2aM97z1llghqyi+XAZRZjyY1l+Wv4=";
     rev = "83c180fd784f3831476d853e06a52082b9b30c70";
     hash = "sha256-kHL9jNukMlIvXM13ytrThHdYbJ92bau+W2zJAUl/E5g=";
   };
 
+  # Disable tests that also fail when run outside nixpkgs context,
+  # likely due to changes in unpinned dependencies
+  patches = [ ./skip_tests.patch ];
+
   build-system = [ setuptools ];
 
-  pythonRelaxDeps = [ "typeguard" ];
+#   pythonRelaxDeps = [ "typeguard" ];
 
   dependencies = [
     psutil
@@ -34,12 +42,28 @@ buildPythonPackage rec {
 
   nativeCheckInputs = [
     coverage
+    pytest
+    pytest-asyncio
   ];
 
   checkPhase = ''
     runHook preCheck
 
-    coverage run -m unittest --verbose --failfast tests/*_unittest.py
+    coverage run -m unittest \
+      --verbose \
+      --failfast \
+      tests/*_unittest.py
+
+    coverage run -m testslide.executor.cli \
+      --format documentation \
+      --show-testslide-stack-trace \
+      --fail-fast \
+      --fail-if-focused \
+      tests/*_testslide.py
+
+    PYTHONPATH="$(pwd)/pytest-testslide:$(pwd):$PYTHONPATH" \
+    coverage run -m pytest \
+      pytest-testslide/tests
 
     runHook postCheck
   '';
