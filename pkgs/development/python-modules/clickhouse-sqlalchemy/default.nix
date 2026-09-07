6e949f31dd32430f9a2fbf0f996789c1dcf06968
João Santos Reis <joao.reis@tweag.io>
2026-09-07 10:41:44 +0100

    nixosTests: add clickhouse-sqlalchemy test

--- pkgs/development/python-modules/clickhouse-sqlalchemy/default.nix
+++ pkgs/development/python-modules/clickhouse-sqlalchemy/default.nix
@@ -9,6 +9,7 @@
   asynch,
   clickhouse-driver,
   nix-update-script,
+  nixosTests,
 }:
 
 buildPythonPackage (finalAttrs: {
@@ -67,7 +68,12 @@ buildPythonPackage (finalAttrs: {
     "clickhouse_sqlalchemy"
   ];
 
-  passthru.updateScript = nix-update-script { };
+  passthru = {
+    tests = {
+      inherit (nixosTests) clickhouse-sqlalchemy;
+    };
+    updateScript = nix-update-script { };
+  };
 
   meta = {
     description = "ClickHouse dialect for SQLAlchemy";
